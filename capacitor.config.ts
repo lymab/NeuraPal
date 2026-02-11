a49a448ec572b0e524bf1a431e15169715c89921
MentiumLabs <support@mentiumlabs.com>
2026-02-11 00:36:07 +0000

    Initial NeuraPal iOS Capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,28 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mentiumlabs.neurapal',
+  appName: 'NeuraPal',
+  webDir: 'www',
+  server: {
+    url: 'https://neurapal.mentiumlabs.com/dashboard.php',
+    cleartext: false
+  },
+  ios: {
+    contentInset: 'automatic',
+    preferredContentMode: 'mobile',
+    scheme: 'NeuraPal',
+    backgroundColor: '#1B2A4A'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#1B2A4A',
+      showSpinner: false,
+      launchFadeOutDuration: 400
+    }
+  }
+};
+
+export default config;
